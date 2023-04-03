e35438899b8aee987dacb4b9e54c8923a92bb130
Nicolas David <nicolasdavid193@gmail.com>
2023-04-03 16:49:03 -0300

    Aula 2

---
+++ src/Pages/Favoritos/index.js
@@ -0,0 +1,20 @@
+import Banner from "components/Banner";
+import styles from "./Favoritos.modules.css";
+
+import React from "react";
+import Titulo from "components/Titulo";
+import Card from "components/Card";
+
+export default function Favoritos() {
+  return (
+    <>
+      <Banner imagem="favoritos" />
+      <Titulo>
+        <h1>Meus Favoritos</h1>
+      </Titulo>
+      <section className={styles.container}>
+        <Card id="1" titulo="Gato bonifacio" capa="" />
+      </section>
+    </>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,22 @@
+import Favoritos from "Pages/Favoritos";
 import Inicio from "Pages/Inicio";
+import Cabecalho from "components/Cabecalho";
+import Container from "components/Container";
+import Rodape from "components/Rodape";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Inicio />} />
-      </Routes>
+      <Cabecalho />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/Favoritos" element={<Favoritos />} />
+        </Routes>
+      </Container>
+
+      <Rodape />
     </BrowserRouter>
   );
 }
